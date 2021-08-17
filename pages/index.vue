6d33099d0953ddbd39d3b5a0bc46499cfb0643e9
Allo <info@allo.dev>
2021-08-17 18:49:11 +0200

    Basic Navbar and Pages with background

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Tutorial/>
+  <div>
+    <section class="first">
+      <div class="nav">
+        <img class="nav-logo" src="~/assets/images/logo.png" />
+        <div class="nav-menu">
+          <a class="nav-item">About</a>
+          <a class="nav-item">Editons</a>
+          <a class="nav-item">Roadmap</a>
+          <a class="nav-item">Provenance</a>
+        </div>
+
+        <a href="something" class="button">Connect Wallet</a>
+      </div>
+    </section>
+    <section class="second"></section>
+    <section class="third"></section>
+    <section class="fourth"></section>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
